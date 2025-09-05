5803b8e9429c962b163bd5e9b48012dda1bf4089
SALLES16 <ariadne.sales@escola.pr.gov.br>
2025-09-05 14:46:06 +0000

    aula3

---
+++ script.js
@@ -0,0 +1,9 @@
+const avanca =ducument.querySelectorAAll(".btn-proximo")
+avanca.forEach(button =>{
+    button.addEvantListener("click"function(){
+    const atual = documento.querySelector("ativo");
+    const proximoPosso = 'passo-' + this.getAtribute('dta-proximo');
+
+    atual.classList.remove('ativo');
+    documento.gatElemant
+    
